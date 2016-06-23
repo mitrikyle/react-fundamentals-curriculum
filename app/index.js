a7f104f09fcf295662a2f6f92a0c606fb11a67f1
Brereton <dmitri.brereton@kbb.com>
2016-06-23 15:43:38 -0700

    made swag

--- app/index.js
+++ app/index.js
@@ -2,14 +2,36 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Hi = React.createClass({
+  swag: function () {
+    this.setState({
+      chi: this.state.chi + "o"
+    });
+  },
+
+  getInitialState: function () {
+    return {
+      chi : ""
+    }
+  },
+
   render: function() {
     return (
-      <div> Test </div>
+      <div>
+      <h1>Hi {this.props.name}{this.state.chi} </h1>
+      <button type="button" onClick={this.swag}>Swagify</button>
+      </div>
     )
   }
 });
 
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <Hi name="bro"/>
+    )
+  }
+})
 ReactDOM.render(
-  <Hi />,
+  <Hello />,
   document.getElementById('app')
 );
